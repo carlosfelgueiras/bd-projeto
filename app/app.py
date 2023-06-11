ca8202d230e0e9a247889063a27337849333f479
carlosfelgueiras <catofel2000@gmail.com>
2023-06-11 22:49:00 +0100

    refactor: templates; feature: error handling on product create

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import Response
 from psycopg.rows import namedtuple_row
 from psycopg_pool import ConnectionPool
 from math import ceil
@@ -41,22 +42,25 @@ dictConfig(
 
 app = Flask(__name__)
 log = app.logger
+app.secret_key = "teste"
 
-@app.route('/', methods=('GET',))
+
+@app.route("/", methods=("GET",))
 def index():
-    return render_template('base.html')
+    return render_template("base.html")
 
-@app.route('/products', methods=('GET',))
+
+@app.route("/products", methods=("GET",))
 def products_index():
     DEFAULT_AMMOUNT = 10
 
-    if request.args.get('p') is None:
-        return redirect(url_for('products_index', p=1))
+    if request.args.get("p") is None:
+        return redirect(url_for("products_index", p=1))
 
-    p = eval(request.args.get('p'))
+    p = eval(request.args.get("p"))
 
     if p < 1:
-        return redirect(url_for('products_index', p=1))
+        return redirect(url_for("products_index", p=1))
 
     with pool.connection() as conn:
         with conn.cursor(row_factory=namedtuple_row) as cur:
@@ -65,67 +69,81 @@ def products_index():
                 SELECT COUNT(*) FROM product;
                 """
             ).fetchone()[0]
-            
-            if DEFAULT_AMMOUNT*(p-1) >= count:
-                return redirect(url_for('products_index', p=ceil(count/DEFAULT_AMMOUNT)))
+
+            if DEFAULT_AMMOUNT * (p - 1) >= count:
+                return redirect(
+                    url_for("products_index", p=ceil(count / DEFAULT_AMMOUNT))
+                )
 
             products = cur.execute(
                 """
                     SELECT * FROM product LIMIT %(limit)s OFFSET %(page)s;
                 """,
-                { "page": DEFAULT_AMMOUNT*(p-1), "limit": DEFAULT_AMMOUNT }
+                {"page": DEFAULT_AMMOUNT * (p - 1), "limit": DEFAULT_AMMOUNT},
             ).fetchall()
-    return render_template('products/index.html', products=products, p=p, last_p=ceil(count/DEFAULT_AMMOUNT));
+    return render_template(
+        "products/index.html",
+        products=products,
+        p=p,
+        last_p=ceil(count / DEFAULT_AMMOUNT),
+    )
 
-@app.route('/products/new', methods=('GET', 'POST'))
+
+@app.route("/products/new", methods=("GET", "POST"))
 def products_new():
     if request.method == "GET":
-        return render_template('products/new.html')
-    
-    if request.method == "POST":
-        if len(request.form["description"]) == 0:
-            return "ERROR"
-        
-        if len(request.form["name"]) == 0 or len(request.form["name"]) > 200:
-            return "ERROR"
+        return render_template("products/new.html")
 
-        if len(request.form["sku"]) == 0 or len(request.form["sku"]) > 25:
-            return "ERROR"
-        
-        if len(request.form["ean"]) > 13:
-            return "ERROR"
-        
-        if not request.form["price"].isnumeric():
-            return "ERROR"
-        
+    if request.method == "POST":
+        # These conditions are enforced in the client side
+        if (
+            len(request.form["description"]) == 0
+            or len(request.form["name"]) == 0
+            or len(request.form["name"]) > 200
+            or len(request.form["sku"]) == 0
+            or len(request.form["sku"]) > 25
+            or len(request.form["ean"]) > 13
+            or not request.form["price"].isnumeric()
+        ):
+            flash("There was an error adding the product. Please try again later.")
+            return redirect(url_for("products_index"))
+
         with pool.connection() as conn:
             with conn.cursor(row_factory=namedtuple_row) as cur:
-
-                if len(request.form["ean"]) == 0:
-                    cur.execute(
-                        """
-                            INSERT INTO product VALUES(%(sku)s, %(name)s, %(description)s, %(price)s);
-                        """,
-                        request.form
+                try:
+                    if len(request.form["ean"]) == 0:
+                        cur.execute(
+                            """
+                                INSERT INTO product VALUES(%(sku)s, %(name)s, %(description)s, %(price)s);
+                            """,
+                            request.form,
+                        )
+                    else:
+                        cur.execute(
+                            """
+                                INSERT INTO product VALUES(%(sku)s, %(name)s, %(description)s, %(price)s, %(ean)s);
+                            """,
+                            request.form,
+                        )
+                except psycopg.errors.UniqueViolation:
+                    flash("A product with the same SKU already exists.")
+                except:
+                    flash(
+                        "There was an error adding the product. Please try again later."
                     )
-                else:
-                    cur.execute(
-                        """
-                            INSERT INTO product VALUES(%(sku)s, %(name)s, %(description)s, %(price)s, %(ean)s);
-                        """,
-                        request.form
-                    )
 
-                return redirect(url_for('products_index'))
-        
+                return redirect(url_for("products_index"))
 
-@app.route('/products/edit/<sku>')
+
+@app.route("/products/edit/<sku>")
 def products_edit(sku):
     pass
 
-@app.route('/products/delete/<sku>')
+
+@app.route("/products/delete/<sku>")
 def products_delete(sku):
     pass
+
 
 if __name__ == "__main__":
     app.run()
